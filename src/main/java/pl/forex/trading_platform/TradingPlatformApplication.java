fb3ad5b62b115bc41723d71c49959ddf30797398
mlipski00 <michal.lipski2@gmail.com>
2018-06-26 15:28:48 +0200

    initial commit

---
+++ src/main/java/pl/forex/trading_platform/TradingPlatformApplication.java
@@ -0,0 +1,12 @@
+package pl.forex.trading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingPlatformApplication.class, args);
+    }
+}
