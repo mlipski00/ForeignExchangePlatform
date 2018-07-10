a63577e00f23828e74e44b8d1234bc0c272672ea
mlipski00 <michal.lipski2@gmail.com>
2018-07-10 22:54:19 +0200

    Configuration for VPS deployment

--- src/main/java/pl/forex/trading_platform/TradingPlatformApplication.java
+++ src/main/java/pl/forex/trading_platform/TradingPlatformApplication.java
@@ -3,14 +3,20 @@ package pl.forex.trading_platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "pl.forex")
 @EnableJpaRepositories
 @EnableAutoConfiguration
-public class TradingPlatformApplication {
+public class TradingPlatformApplication  extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TradingPlatformApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(TradingPlatformApplication.class, args);
 
