55144943748f7f198dd3ad3e9b2c6c7ff80acf1d
mlipski00 <michal.lipski2@gmail.com>
2018-07-14 21:26:49 +0200

    frontend development

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,52 +1,63 @@
-var stompClient = null;
+window.onload = function () {
+    var stompClient = null;
+    var appendedQuotation = document.getElementById("quotationRow");
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
+    function setConnected(connected) {
+        $("#connect").prop("disabled", connected);
+        $("#disconnect").prop("disabled", !connected);
+        if (connected) {
+            $("#conversation").show();
+        }
+        else {
+            $("#conversation").hide();
+        }
+        $("#userinfo").html("");
     }
-    else {
-        $("#conversation").hide();
-    }
-    $("#userinfo").html("");
-}
 
-function connect() {
-    var socket = new SockJS('websocket');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/user', function (greeting) {
-            showGreeting(JSON.parse(greeting.body));
+    function connect() {
+        var socket = new SockJS('websocket');
+        stompClient = Stomp.over(socket);
+        stompClient.connect({}, function (frame) {
+            setConnected(true);
+            console.log('Connected: ' + frame);
+            stompClient.subscribe('/topic/user', function (greeting) {
+                showGreeting(greeting.body);
+                $("#quotationRow").hide();
+                $("#quotationRow").fadeIn("1000");
+            });
+        });
+    }
+
+    function disconnect() {
+        if (stompClient !== null) {
+            stompClient.disconnect();
+        }
+        setConnected(false);
+        console.log("Disconnected");
+    }
+
+
+    function showGreeting(quotation) {
+        var json = JSON.parse(quotation);
+        console.log(json);
+        console.log(json[0].time);
+        appendedQuotation.innerHTML = "";
+        for (var i = 0; i < json.length; i++) {
+            appendedQuotation.innerHTML += "<tr><td>" + json[i].instrument.description
+                + "</td><td>" + json[i].time + "</td><td>" + json[i].bidPriceBucket.price
+            +"</td><td>"+ json[i].askPriceBucket.price + "</td></tr>";
+        }
+    }
+
+    $(function () {
+        $("form").on('submit', function (e) {
+            e.preventDefault();
+        });
+        $("#connect").click(function () {
+            connect();
+        });
+        $("#disconnect").click(function () {
+            disconnect();
         });
     });
 }
-
-function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-    stompClient.send("/app/user", {}, JSON.stringify({'name': $("#name").val()}));
-}
-
-function showGreeting(message) {
-    $("#userinfo").append("<tr><td>" + JSON.stringify(message) + "</td></tr>");
-    $('html,body').animate({scrollTop: document.body.scrollHeight},"fast");
-
-}
-
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
-});
